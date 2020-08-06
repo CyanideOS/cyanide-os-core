6062c8ac1e149cb29ebb0ae7ebdc50bbeeb22eec
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-08-06 10:58:23 +0530

    Added rmdir

--- storage/utilities/rmdir.cpp
+++ storage/utilities/rmdir.cpp
@@ -4,7 +4,6 @@
 int main(int argc, char const* argv[])
 {
     namespace fs = std::filesystem;
-    std::cout << "Hello 2" << std::endl;
-    // fs::create_directory(argv[1]);
+    fs::remove_all(argv[1]);
     return 0;
 }
